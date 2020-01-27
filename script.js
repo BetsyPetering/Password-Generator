6a7ff2ed2bb3d9463bbe64dfaf85a22708e88634
PinkAttitude77 <betsy.petering@sbcglobal.net>
2020-01-26 22:42:12 -0800

    html and css mostly done, js begun and working on possibilities

--- script.js
+++ script.js
@@ -1,23 +1,120 @@
-//I've only just begun.......
+//Set confirmation variables to false
+var useNum = confirm("Should Password Contain Numbers?");
+var useSym = confirm("Should Password Contain Symbols?");
+var useUCs = confirm("Should Password Contain Upper Case Letters?");
+var useLCs = confirm("Should Password Contain Lower Case Letters?");
+
+console.log(useNum);
+console.log(useSym);
+console.log(useUCs);
+console.log(useLCs);
+
+if ( (useNum = true) || (useSym = true) || (useUCs = true) || (useLCs = true) ) {
 
 
-const charNum = document.getElementById('charNum')
-const upperCase = document.getElementById('upperCase')
-const lowerCase = document.getElementById('lowerCase')
-const numbers = document.getElementById('numbers')
-const symbols = document.getElementById('symbols')
-const form = document.getElementById('pswdForm')          
+console.log(useNum);
+console.log(useSym);
+console.log(useUCs);
+console.log(useLCs);
+
+console.log("got to here")
+
+
+var checkBoxes = document.querySelector("#make");
+
+//Create arrays for symbols, upper case letters, and lower case letters
+var symbolArr = ["!", "0", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "]", "^", "_", "`", "{", "|", "}", "~"]
+var upperCaseArr = ["A", 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+var lowerCaseArr = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
 
 
 
-form.addEventListener('submit', function(e) {
-    e.preventDefault()
-    const charNum = charNumNumber.value
-    const upperCase = includeUpperCaseElement.checked
-    const lowerCase = includeLowerCaseElement.checked
-    const numbers = includeNumbersElement.checked
-    const symbols = includeSymbols.checked
-
-    const password = generatePassword(charNum, upperCase, lowerCase, numbers, symbols)
 }
+else {
+    alert("Because no type of character was selected, no Password for You!");
+}
+
+//Listen for "Generate Password" click
+
+// genPass.addEventListener("click", function(event) {
+//   window.location.href = images[index];
+// });
+
+// checkBoxes.addEventListener("click", function(event) {
+//         event.preventDefault();
+//         ;
+//         });
+
+
+// To make a number
+//for (var i = 0; i < 10; i++) {
+    // Generate a random number between 1 and 10
+//     var number = Math.floor(Math.random() * 10) + 1;
+// }
+
+
+//Choose the array number that the symbol will be
+// for (var i = 0; i < symbolArr.length; i++) {
+//     var number = Math.floor(Math.random() * symbolArr.length) + 1;
+// }
+
+//listen for the options chosen
+      
+
+
+
+
+
+      // Loop the number of times needed
+    //   for (var i = 0; i < 10; i++) {
+        // Generate a random number between 1 and 10
+        // Math.floor will round down, meaning we would get a number between 0 and 9, so we'll always add 1 to bump it up.
+        // var num = Math.floor(Math.random() * 10) + 1;
+
+        // Display in console
+        // console.log(num);
+    //   }
+
+
+// var charNum = document.getElementById('charNum')
+// var upperCase = document.getElementById('upperCase')
+// var lowerCase = document.getElementById('lowerCase')
+// var numbers = document.getElementById('numbers')
+// var symbols = document.getElementById('symbols')
+// var form = document.getElementById('pswdForm')          
+
+
+
+
+// form.addEventListener('submit', function(e) {
+//     e.preventDefault()
+//     var charNum = charNumNumber.value
+//     var upperCase = includeUpperCaseElement.checked
+//     var lowerCase = includeLowerCaseElement.checked
+//     var numbers = includeNumbersElement.checked
+//     var symbols = includeSymbols.checked
+
+//     var password = generatePassword(charNum, upperCase, lowerCase, numbers, symbols);
+// }
+
+
+
+
+
+
+//So, I really wanted to make this start with a do while loop so if they kept
+//answering no to any of the character types, it would just ask them again
+//But I can't make this work, so on to the next one
+// do {
+//     useNum = confirm("Should Password Contain Numbers?");
+//     useSym = confirm("Should Password Contain Symbols?");
+//     useUCs = confirm("Should Password Contain Upper Case Letters?");
+//     useLCs = confirm("Should Password Contain Lower Case Letters?");
+// }
+//     while ((useNum = false) && (useSym = false) && (useUCs = false) && (useLCs = false));
+
+// console.log(useNum);
+// console.log(useSym);
+// console.log(useUCs);
+// console.log(useLCs);
