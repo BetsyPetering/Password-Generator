551b7c9c527f6728a64cd8bbfcb6d1d67b84d0cb
PinkAttitude77 <betsy.petering@sbcglobal.net>
2020-02-04 23:49:28 -0800

    new random generator

--- script.js
+++ script.js
@@ -1,95 +1,102 @@
-//Set confirmation variables to false
+// TA's comments from originally handed in work:
 
-var useNum = confirm("Should Password Contain Numbers?");
-var useSym = confirm("Should Password Contain Symbols?");
-var useUCs = confirm("Should Password Contain Upper Case Letters?");
-var useLCs = confirm("Should Password Contain Lower Case Letters?");
+//Change the variable initializing to checkbox booleans
+// A good place to get started in this sort of assignment is to first 
+//     (1) lay out all the things you are going to use, like buttons, input fields, etc. 
+//     (2) list all the functions you will need to write
 
-console.log('useNum = ' + useNum);
-console.log('useSym = ' + useSym);
-console.log('useUCs = ' + useUCs);
-console.log('useLCs = ' + useLCs);
+// var inputButton = document.getElementById('input-button'); // etc etc
+// var lowercaseLetters = "abcdefghijklmnopqrstuvwxyz";
+// function generatePassword(options) { } // haven't written yet
+//----------------------------------------------------------------------------------------------------------------------------------------
 
-//test if user entered any type of character, only go on if they have chosen one or more
-if ((useNum === true) || (useSym === true) || (useUCs === true) || (useLCs === true)) {
-    console.log('line 14');
+//Create arrays
+var types = ["number", "symbol", "upper", "lower"];
+var number = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+var symbol = ["!", "0", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "]", "^", "_", "`", "{", "|", "}", "~"];
+var upper = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+var lower = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+var array = ['types', 'number', 'symbol', 'upper', 'lower'];
+var passWord = [];
 
-    //Ask for their number of digits in this password between 8 and 128
-    var pswdLength = prompt("Passwords will be from 8 to 128 characters long. How many characters do you want?");
-    console.log('pswdLength = ' + pswdLength);
+//QUESTION:  which way to create the array of digit types? By make each one a boolean of true for using it or false for not? 
+//                          Or by the system below of adding the type to an array - then use that array for the pool from which to choose the next digit type
 
-    //Make sure number is from 8 to 128
-    if ((pswdLength >= 8) && (pswdLength <= 128)); {
-        console.log('line 22 ');
+//create the array with the types of characters to use
 
-        var arrLen = 0;
-        var types = [];
-        //create the array with the types of characters to use
-        if (useNum === true) {
-            types[arrLen] = "number";
-            arrLen++;
-        }
-        if (useSym === true) {
-            types[arrLen] = "symbol";
-            arrLen++;
-        }
-        if (useUCs === true) {
-            types[arrLen] = "upper";
-            arrLen++;
-        }
-        if (useLCs === true) {
-            types[arrLen] = "lower";
-            arrLen++;
-        }
-        console.log('array types = ' + types);
-        console.log('line 45 arrLen = ' + arrLen);
+function genPassword() {
+     
+    var digits = document.getElementById('length');
+    var number = document.getElementById('input-number');
+    var symbol = document.getElementById('input-symbol');
+    var upper = document.getElementById('input-upper');
+    var lower = document.getElementById('input-lower');
+    console.log("digits = " + digits.value);
+    console.log("number = " + number.value);
+    console.log("symbol = " + symbol.value);
+    console.log("upper = " + upper.value);
+    console.log("lower = " + lower.value);
 
-        //At this point types is the array holding the types of characters we will use
+    var arrLen = 0;
+    var types = [];
 
-        //Create arrays for symbols, upper case letters, and lower case letters
-       // for the numbers, we will use the random number generator to choose from 0 - 9
-        var symbolArr = ["!", "0", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "]", "^", "_", "`", "{", "|", "}", "~"];
-        var upperCaseArr = ["A", 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
-        var lowerCaseArr = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
-       
-        //make the array for the password
-        var passWord = [];
-        console.log('line 57 (new array should be empty) passWord array = ' + passWord);
+    console.log("before making types[] ");
 
-        //start making a password! Length of Password is "pswdLength"
-        for (var i = 0; i < pswdLength; i++) {
-
-            //determine the type of character to make by picking the node of the array called types
-            var node = Math.floor(Math.random() * types.length);
-            console.log('line 64 node = ' + node);
-
-            //use node to pull the Type of character to pick at random
-            if (types[node] === "number") {
-                console.log('making a number');
-                passWord[i] = Math.floor(Math.random() * 10) + 1;
-                console.log('passWord node ' + i + ' is ' + passWord[i]);
-            }
-            else if (types[node] === "symbol") {
-                console.log('making a symbol');
-                s = Math.floor(Math.random() * symbolArr.length);
-                passWord[i] = symbolArr[s];
-                console.log('passWord node ' + i + ' is ' + passWord[i]);
-            }
-            else if (types[node] === "upper") {
-                console.log('making a upper');
-                u = Math.floor(Math.random() * upperCaseArr.length);
-                passWord[i] = upperCaseArr[u];
-                console.log('passWord node ' + i + ' is ' + passWord[i]);
-            }
-            else {
-                console.log('making a lower');
-                l = Math.floor(Math.random() * lowerCaseArr.length);
-                passWord[i] = lowerCaseArr[l];
-                console.log('passWord node ' + i + ' is ' + passWord[i]);
-            }
-            console.log('Final password[' + i + '] = ' + passWord[i]);
-        }
+    if (number.value) {
+        types[arrLen] = "number";
+        arrLen++;
     }
-}
-///Final Password is now in passWord array
+    if (symbol.value) {
+        types[arrLen] = "symbol";
+        arrLen++;
+    }
+    if (upper.value) {
+        types[arrLen] = "upper";
+        arrLen++;
+        console.log("I'm in upper");
+    }
+    if (lower.value) {
+        types[arrLen] = "lower";
+        arrLen++;
+    }
 
+    console.log("after making types[] ");
+
+    console.log("types array = " + types);
+
+    //create a function that will return a random number
+    //in our case, we will use this as the node of the given array to choose
+
+    function rand(arr) {        
+        console.log("inside rand function");
+        var node = Math.floor(Math.random() * arr.length);
+        return node;
+    }
+    
+    //run through the randomizing functions, one to choose the array, the next to 
+    //choose the node, and do this for as many digits as were chosen
+
+    for (var i = 0; i < digits.value; i++) {
+
+        console.log("inside the for loop");
+        console.log("i = " + i);
+
+        //t  will be the node of types[] to use next
+        var t = rand(types);
+
+        //use t to extract the next character array
+        var nextArr = types[t];
+
+        //use the rand function to choose the node of the character array 
+        place = rand(nextArr);
+
+        //make the next digit of the password node "place" in "nextArr" array
+        passWord[i] = nextArr[place];
+
+        console.log("passWord[i] = " + passWord[i]);
+        console.log("this is place = " + place);
+    }
+    alert("passWord = " + passWord);
+}
+
+document.getElementById("make").addEventListener("click", genPassword);
